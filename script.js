68f1a1983ade196972cb7501b2089c53527fe6ce
Hossein Nabi <68454951+hosseinnabi-ir@users.noreply.github.com>
2021-10-29 13:38:46 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const sec = document.querySelector('p #sec');
+const min = document.querySelector('p #min');
+const hr = document.querySelector('p #hr');
+
+const start = document.querySelector('.container #start');
+const stop = document.querySelector('.container #stop');
+const reset = document.querySelector('.container #reset');
+
+let Interval;
+let ss = 0;
+let mm = 0;
+let hh = 0;
+
+// Start Button Click Event
+start.addEventListener('click', () => {
+
+    Interval = setInterval(() => {
+        if(ss < 59){
+            ss += 1;
+            sec.innerHTML = ss < 10 ? '0' + ss : ss;
+        }
+        else {
+            ss = 0;
+            sec.innerHTML = ss < 10 ? '0' + ss : ss;
+            if(mm < 59){
+                mm += 1;
+                min.innerHTML = mm < 10 ? '0' + mm : mm;
+            } 
+            else {
+                hh += 1;
+                hr.innerHTML = hh < 10 ? '0' + hh : hh;
+            }
+        }
+    }, 1000);
+
+    start.style.pointerEvents = 'none';
+
+});
+
+// Stop Button Click Event
+stop.addEventListener('click', () => {
+    clearInterval(Interval);
+    start.style.pointerEvents = 'visible';
+});
+
+// Reset Button Click Event
+reset.addEventListener('click', () => {
+    location.reload();
+})
